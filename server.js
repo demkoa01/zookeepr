33a0ec7a91e30d491ebc7b33d4aec75cc7145727
Allison Demkovich <demkovich.allison@gmail.com>
2022-02-09 15:16:49 -0600

    add front end development for site

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const { type } = require('os');
 app.use(express,urlencoded({ extended:true }));
 // parse incoming JSON data
 app.use(express.json());
+// make files in public folder available
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -106,6 +108,22 @@ app.post('/api/animals', (req,res) => {
         res.json(animal);
     }
 
+});
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('./zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
